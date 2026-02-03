73ff5c6c62d127d5f153b9aaf45be7f467692ec1
TPL-2004 <tharinipoornimalelwala@gmail.com>
2026-02-03 11:26:33 +0530

    cleaned the file structure

--- backend/db/migrations/002_add_file_name_to_teacher_verifications.sql
+++
@@ -1,2 +0,0 @@
--- Add file_name column to teacher_verifications table
-ALTER TABLE teacher_verifications ADD COLUMN IF NOT EXISTS file_name VARCHAR(255);
--- backend/db/migrations/003_add_user_profile_fields.sql
+++
@@ -1,5 +0,0 @@
--- Add firstname, lastname, and birthday columns to users table if they don't exist
-ALTER TABLE users
-ADD COLUMN IF NOT EXISTS firstname VARCHAR(100),
-ADD COLUMN IF NOT EXISTS lastname VARCHAR(100),
-ADD COLUMN IF NOT EXISTS birthday DATE;
